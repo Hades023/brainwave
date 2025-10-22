dbddaa5fc2d90662f12630c6f4d878b9e1532234
Hades023 <ifraire023@gmail.com>
2025-10-21 20:44:00 -0500

    On this date we finalized NavBar and created roots. We also began to add/display main content.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
+import Navbar from "./components/layout/Navbar.jsx";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="min-h-dvh flex flex-col">
+        <Navbar />
+        {/* main content and footer will go here */}
+        <Outlet />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,20 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
 
-const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    childern: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
